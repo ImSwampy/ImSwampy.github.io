fab137ea7add0a04ebb52ff48004f86cb7354ee8
ImSwampy <tijaguar44@gmail.com>
2025-03-30 16:05:13 +0200

    fix the website

---
+++ _app/immutable/nodes/0.BaKzuGT4.js
@@ -0,0 +1,1 @@
+import{c as s,a as f}from"../chunks/G_QxcWcY.js";import{b as i,E as c,c as p,n as d,e as m,h,d as l,f as u}from"../chunks/NitZzSDk.js";function v(e,t,...a){var n=e,o=d,r;i(()=>{o!==(o=t())&&(r&&(m(r),r=null),r=p(()=>o(n,...a)))},c),h&&(n=l)}function E(e,t){var a=s(),n=u(a);v(n,()=>t.children),f(e,a)}export{E as component};
